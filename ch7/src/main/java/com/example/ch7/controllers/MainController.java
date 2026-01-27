a8354c160a5fd468625f7444885b5c68b6ea2c99
the25thjin <mannr075@gmail.com>
2026-01-27 21:14:35 +0530

    leared abt request paramter

--- ch7/src/main/java/com/example/ch7/controllers/MainController.java
+++ ch7/src/main/java/com/example/ch7/controllers/MainController.java
@@ -1,13 +1,19 @@
 package com.example.ch7.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(@RequestParam String name,
+                       @RequestParam String color,
+                       Model page){
+        page.addAttribute("username",name);
+        page.addAttribute("color" , color);
         return "home.html";
     }
 }
