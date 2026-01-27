f1c04d281db9a51c86914bf1eff735a9d68ccef3
the25thjin <mannr075@gmail.com>
2026-01-27 21:18:35 +0530

    leared abt path variable

--- ch7/src/main/java/com/example/ch7/controllers/MainController.java
+++ ch7/src/main/java/com/example/ch7/controllers/MainController.java
@@ -3,16 +3,17 @@ package com.example.ch7.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String home(@RequestParam String name,
-                       @RequestParam String color,
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color,
                        Model page){
-        page.addAttribute("username",name);
+        page.addAttribute("username","Mann");
         page.addAttribute("color" , color);
         return "home.html";
     }
